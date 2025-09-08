324f52ac0ccca95e3776b63ba463e436b2671727
prasadshivendra54/ <tshivendra07@gmail.com>
2025-09-08 21:23:56 +0530

    updated code for production ready

--- client/src/Routes/Routing.tsx
+++ client/src/Routes/Routing.tsx
@@ -1,21 +1,38 @@
-import React from "react";
+// import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import HomePage from "../components/HomePage";
 import Signin from "../components/Signin";
 import Signup from "../components/Signup";
 import Dashboard from "../components/Dashboard";
+import { type ReactNode } from "react";
 
+
+
+// This code not work in production
+// // Protected route component
+// // This will checks if the user is logged in. if no token is found, redirect them to signin page to signin. and if token exists, allow to redirect dashboard page.
+// const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
+//   const token = localStorage.getItem("userToken"); // check token in localStorage 
+//   if (!token) {
+//     return <Navigate to="/signin" replace />;
+//   }
+//   return children;
+// };
+
+
+// It's work
 
 // Protected route component
-// This will checks if the user is logged in. if no token is found, redirect them to signin page to signin. and if token exists, allow to redirect dashboard page.
-const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
-  const token = localStorage.getItem("userToken"); // check token in localStorage 
+// Checks if the user is logged in. If no token is found, redirects to signin page. Otherwise, renders children.
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = localStorage.getItem("userToken"); // check token in localStorage
   if (!token) {
     return <Navigate to="/signin" replace />;
   }
-  return children;
+  return <>{children}</>;
 };
+
 
 // Routing
 const Routing = () => {
